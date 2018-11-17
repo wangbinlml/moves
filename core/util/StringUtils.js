289051213ed504e51fa624d6e753d2329bffe803
wang <wangbinlml@163.com>
2018-11-17 15:48:29 +0800

    html标签转义和反转义
    html标签转义和反转义

--- core/util/StringUtils.js
+++ core/util/StringUtils.js
@@ -123,3 +123,64 @@ module.exports.htmlDecodeByRegExp = function (str){
     s = s.replace(/&quot;/g,"\"");
     return s;
 }
+
+
+/**
+ * 将str中的html符号转义,将转义“'，&，<，"，>”五个字符
+ * @method unhtml
+ * @param { String } str 需要转义的字符串
+ * @return { String } 转义后的字符串
+ * @example
+ * ```javascript
+ * var html = '<body>&</body>';
+ *
+ * //output: &lt;body&gt;&amp;&lt;/body&gt;
+ * console.log( UE.utils.unhtml( html ) );
+ *
+ * ```
+ */
+module.exports.unhtml=function (str, reg) {
+    return str ? str.replace(reg || /[&<">'](?:(amp|lt|quot|gt|#39|nbsp|#\d+);)?/g, function (a, b) {
+        if (b) {
+            return a;
+        } else {
+            return {
+                '<':'&lt;',
+                '&':'&amp;',
+                '"':'&quot;',
+                '>':'&gt;',
+                "'":'&#39;'
+            }[a]
+        }
+
+    }) : '';
+};
+
+/**
+ * 将str中的转义字符还原成html字符
+ * @see UE.utils.unhtml(String);
+ * @method html
+ * @param { String } str 需要逆转义的字符串
+ * @return { String } 逆转义后的字符串
+ * @example
+ * ```javascript
+ *
+ * var str = '&lt;body&gt;&amp;&lt;/body&gt;';
+ *
+ * //output: <body>&</body>
+ * console.log( UE.utils.html( str ) );
+ *
+ * ```
+ */
+module.exports.html=function (str) {
+    return str ? str.replace(/&((g|l|quo)t|amp|#39|nbsp);/g, function (m) {
+        return {
+            '&lt;':'<',
+            '&amp;':'&',
+            '&quot;':'"',
+            '&gt;':'>',
+            '&#39;':"'",
+            '&nbsp;':' '
+        }[m]
+    }) : '';
+};
